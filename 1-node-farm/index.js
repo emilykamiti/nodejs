03f77c4f6b8df523e81c32dc55ecb0d74d552d77
emilykamiti <emilykamiti@gmail.com>
2024-06-17 16:28:27 +0300

    Routing building a simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,9 +1,10 @@
 
-// const hello = 'Hello world';
-// console.log(hello);
-//
- const fs = require('fs');
-//
+const fs = require('fs');
+const http  = require('http');
+const url = require('url');
+
+///////////////////////////////
+//FILES
 ////synchronous way - blocking
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -11,20 +12,48 @@
 // const textOut = 'This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}';
 // fs.writeFileSync('./txt/output', textOut);
 // console.log('file written!');
+//
+////Asynchronous way- non blocking
+//fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//            console.log(data3);
+//
+//            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//            console.log('your file has been written');
+//            })
+//        });
+//    });
+//});
+////////////////////////
 
-//Asynchronous way- non blocking
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            console.log(data3);
+//SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj=JSON.parse(data);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-            console.log('your file has been written');
-            })
-        });
-    });
+//above code , executed once ,only when the code is run
+const server = http.createServer((req, res)=> {
+const pathName = req.url;
 
+
+if(pathName === '/' || pathName === '/overview'){
+res.end('This is the OVERVIEW');
+} else if (pathName === '/product'){
+console.log('reaching here');
+res.end('This is the PRODUCT');
+}else if (pathName === '/api'){
+res.writeHead(200, {'Content-type': 'application/json'});
+res.end(data);
+} else{
+res.writeHead(404, {
+'Content-type': 'text/html',
+'my-own-header': 'hello-world'
 });
-
+res.end('<h1>page not found<h1>');
+}
+});
+server.listen(8000, '127.0.0.1', () => {
+console.log('listening to request on port 8000!')
+});
 
 
