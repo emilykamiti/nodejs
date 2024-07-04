a2338a93aa9b21a3ede1f7046870534efbac151f
emilykamiti <emilykamiti@gmail.com>
2024-07-04 19:02:38 +0300

    Routing- implementing differnt action for different urls Using a simple api - where we required data from Templates - Manipulating html templates Parsing variables from the url Using Modules 2 our own modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,8 @@
 
 const fs = require('fs');
-const http  = require('http');
+const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////
 //FILES
@@ -28,32 +29,52 @@ const url = require('url');
 ////////////////////////
 
 //SERVER
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj=JSON.parse(data);
+
+const dataObj = JSON.parse(data);
 
 //above code , executed once ,only when the code is run
-const server = http.createServer((req, res)=> {
-const pathName = req.url;
+const server = http.createServer((req, res) => {
+
+    const { query, pathname } = url.parse(req.url, true);
 
 
-if(pathName === '/' || pathName === '/overview'){
-res.end('This is the OVERVIEW');
-} else if (pathName === '/product'){
-console.log('reaching here');
-res.end('This is the PRODUCT');
-}else if (pathName === '/api'){ //added a simple api
-res.writeHead(200, {'Content-type': 'application/json'});
-res.end(data);
-} else{
-res.writeHead(404, {
-'Content-type': 'text/html',
-'my-own-header': 'hello-world'
-});
-res.end('<h1>page not found<h1>');
-}
+    //overview page
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+        //product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+
+        //API
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+
+        //Not found
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+
+        res.end('<h1>page not found<h1>');
+    }
 });
 server.listen(8000, '127.0.0.1', () => {
-console.log('listening to request on port 8000!')
+    console.log('listening to request on port 8000!');
 });
 
 
