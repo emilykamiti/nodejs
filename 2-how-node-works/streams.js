9c9957e6f810f48384589270c20e8a349b957317
emilykamiti <emilykamiti@gmail.com>
2024-07-26 11:07:04 +0300

    streams

---
+++ 2-how-node-works/streams.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  //solution 1
+
+  //   fs.readFile('test-file.txt', (err, data) => {
+  //     if (err) console.log('err');
+  //     res.end(data);
+  //   });
+
+  //solution2:streams
+  //   const readable = fs.createReadStream('test-file.txt');
+  //   readable.on('data', (chunk) => {
+  //     res.write(chunk);
+  //   });
+  //   readable.on('end', () => {
+  //     res.end();
+  //   });
+
+  //   readable.on('error', (err) => {
+  //     console.log(err);
+  //     res.statuCode = 500;
+  //     res.end('file not found');
+  //   });
+  // });
+
+  //solution 3
+  const readable = fs.createReadStream('test-file.txt');
+  readable.pipe(res); //! Recommended way of using streams, pipe method.
+});
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening...');
+});
